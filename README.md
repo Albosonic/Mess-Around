7f7e39e3cab5154c9ba19fe2d773597de9468952
El_Gato <albertomadueno@gmail.com>
2016-04-29 22:57:34 -0700

    first commit on this repo

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+_____________READ____________ME________________READ__________________ME____________
+
+This is a README theres not much to read about.
+
+This program was created as a learning tool to screw around with.
+
+I hope we learn alot!!!!!!!!!
---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+var first = document.getElementById("first-header");
+var second = document.getElementById("second-header");
+var third = document.getElementById("third-header");
+var fourth = document.getElementById("fourth-header");
+
+// var firstButton = document.getElementById("firstButton");
+
+first.innerHTML = 'What!!';
+second.innerHTML = 'now';
+third.innerHTML = 'bra!';
+fourth.innerHTML = 'pizza??';
+
+// button functions
+function buttonClick1(){
+ return first.innerHTML = 'hi there!! I am a button.';
+};
+
+function buttonClick2(){
+ return second.innerHTML = 'hi there!! I too am a button.';
+};
+
+function buttonClick3(){
+ return third.innerHTML = 'hi there!! I three am a button.';
+};
+
+function buttonClick4(){
+ return fourth.innerHTML = 'hi there!! I four am a button.';
+};
